2b387d654d29161b7ab03e0b83492d9e52dd3fde
JFarand <owlasylum@gmail.com>
2017-05-06 12:46:56 -0400

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -40,15 +40,15 @@ app.get('/todos/:id', (req, res) => {
    var id = req.params.id;
 
    if(!ObjectID.isValid(id)){
-         return res.status(400).send();
+         return res.status(404).send();
    }
 
-   Todo.findById(id).then((todos) => {
-      if(!todos){
+   Todo.findById(id).then((todo) => {
+      if(!todo){
          return res.status(404).send();
       }
 
-      res.send({todos});
+      res.send({todo});
    }, (e) => {
       res.status(400).send();
    })
